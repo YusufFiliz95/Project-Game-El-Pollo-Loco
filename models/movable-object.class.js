42460a8eaf10a425aff2c1e70d9c0dad36c83124
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-03-09 16:50:06 +0100

    added the endboss

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -36,4 +36,12 @@ class MovableObject {
             this.x -= this.speed;
         }, 1000 / 60)
     }
+
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 1, Rest 1
+        // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
 }
