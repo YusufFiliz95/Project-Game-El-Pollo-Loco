0b367e6b491de88c2d2e94d2fdd61384358f4568
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-03-09 16:04:15 +0100

    added running sound

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,9 @@
-class Character extends MovableObject{
+class Character extends MovableObject {
 
     width = 150;
     height = 300;
     y = 135;
-    speed = 10;
+    speed = 5;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -13,41 +13,62 @@ class Character extends MovableObject{
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
-    
+    walking_sound = new Audio('audio/running.mp3');
 
 
 
-    constructor(){
+    constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
 
-    this.animate();
+        this.animate();
 
     }
 
-    animate(){
+    animate() {
+
+        let isWalking = false;
 
         setInterval(() => {
-            if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+            if (!isWalking) {
+                this.walking_sound.pause();
+            }
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.playbackRate = 2;
+                if (!isWalking) {
+                    this.walking_sound.play();
+                    isWalking = true;
+                }
             }
-
-            if(this.world.keyboard.LEFT && this.x > - 600) {
+            if (this.world.keyboard.LEFT && this.x > -600) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.playbackRate = 2;
+                if (!isWalking) {
+                    this.walking_sound.play();
+                    isWalking = true;
+                }
+            }
+            if (!this.world.keyboard.LEFT && !this.world.keyboard.RIGHT) {
+                this.walking_sound.pause();
+                isWalking = false;
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
+        
+        
+        
 
         setInterval(() => {
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            //Walk animation
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 1, Rest 1
-            // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                //Walk animation
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 1, Rest 1
+                // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
             }
         }, 55);
     }
